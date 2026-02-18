70a7624e97d268ae517f7ef5dcf533aaa1c3f543
Junior Dias <juniordiasjfd@gmail.com>
2026-02-18 08:54:30 -0300

    home + logo

--- core/views.py
+++ core/views.py
@@ -8,7 +8,7 @@ from .forms import ConfiguracaoOEDForm
 
 
 class HomeView(TemplateView):
-    template_name = 'base.html'
+    template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
