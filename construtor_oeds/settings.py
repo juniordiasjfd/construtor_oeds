5bbdde255d170ac935ce7e195d47deb2ca7b2f0d
Junior Dias <juniordiasjfd@gmail.com>
2026-02-19 13:11:33 -0300

    gunicorn e whitenoise

--- construtor_oeds/settings.py
+++ construtor_oeds/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -159,6 +160,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Esta pasta será criada automaticamente pelo Render durante o build
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
