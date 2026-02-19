4688f0fd887624d81975ee3428ec3fdfb0ccb2e9
Junior Dias <juniordiasjfd@gmail.com>
2026-02-19 17:12:24 -0300

    fix media render

--- construtor_oeds/urls.py
+++ construtor_oeds/urls.py
@@ -33,4 +33,4 @@ urlpatterns = [
 
 # Servir arquivos de mídia e estáticos durante o desenvolvimento
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
