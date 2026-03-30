cd71e3a8a08adc7a42741e3b7b3a79a8422318c2
Junior Dias <juniordiasjfd@gmail.com>
2026-03-30 16:12:30 -0300

    @admin.register(StatusOed)

--- projetos/admin.py
+++ projetos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import StatusOed
 
-# Register your models here.
+@admin.register(StatusOed)
+class StatusOedAdmin(admin.ModelAdmin):
+    # Campos que aparecerão na lista do Admin
+    list_display = ('nome', 'only_coordenador_can_edit')
+    search_fields = ('nome',)
+    list_filter = ('only_coordenador_can_edit',)
+    ordering = ('nome',)
